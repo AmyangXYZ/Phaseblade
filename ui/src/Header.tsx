5f72d31ced1d3b3bc3473f8b3e28f60853b81d71
AmyangXYZ <amyang.xyz@gmail.com>
2024-11-24 17:01:24 -0500

    adding statusbar

--- ui/src/Header.tsx
+++ ui/src/Header.tsx
@@ -2,14 +2,13 @@ import Card from "./components/Card"
 
 function Header() {
   return (
-    <div className="header glow">
+    <div className="header">
       <a href="https://github.com/AmyangXYZ/Phaseblade" target="_blank">
         <span className="glow">PHASEBLADE</span>
       </a>
 
       <div style={{ display: "flex", gap: "10px" }}>
-        <Card body={`Nodes: 20`} outlineColor="#858585" />
-        <Card body={`Cycles: 100`} outlineColor="#858585" />
+        <Card body={`Protocol: TSCH`} outlineColor="#858585" />
       </div>
     </div>
   )
--- ui/src/StatusBar.tsx
+++ ui/src/StatusBar.tsx
@@ -1,3 +1,18 @@
+import { NotificationsNone } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
+
 export default function StatusBar() {
-  return <div className="status-bar glow">Status</div>
+  return (
+    <div className="status-bar">
+      <div className="status-bar-left">
+        <div className="status-bar-item">Cycle: 100</div>
+        <div className="status-bar-item">Nodes: 20</div>
+      </div>
+      <div className="status-bar-right">
+        <IconButton>
+          <NotificationsNone />
+        </IconButton>
+      </div>
+    </div>
+  )
 }
