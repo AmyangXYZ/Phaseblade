38363b75891bf8aaa9f9533142f00b87ec157faa
AmyangXYZ <amyang.xyz@gmail.com>
2024-11-24 22:52:59 -0500

    adding tooltip

--- ui/src/StatusBar.tsx
+++ ui/src/StatusBar.tsx
@@ -1,12 +1,23 @@
 import { NotificationsNone } from "@mui/icons-material"
-import { IconButton } from "@mui/material"
+import { IconButton, Tooltip } from "@mui/material"
 
 export default function StatusBar() {
   return (
     <div className="status-bar">
       <div className="status-bar-left">
         <div className="status-bar-item">Cycle: 100</div>
-        <div className="status-bar-item">Nodes: 20</div>
+        <Tooltip
+          title={
+            <div className="tooltip-content">
+              <h3>Nodes</h3>
+              {/* Add any other components or styling */}
+            </div>
+          }
+          arrow
+          enterDelay={0}
+        >
+          <div className="status-bar-item">Nodes: 20</div>
+        </Tooltip>
       </div>
       <div className="status-bar-right">
         <IconButton>
